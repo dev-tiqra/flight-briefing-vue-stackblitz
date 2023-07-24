8b18b3eb4b7acadbf20759ebfaa6aa62ddaa9e21
dev-tiqra <dev.tiqra@gmail.com>
2023-07-24 16:37:03 +0200

    1 binaries uploaded

--- README.md
+++
@@ -1,3 +0,0 @@
-# vue3-script-setup-with-vite
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue3-script-setup-with-vite)
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
-<script setup>
-import { ref } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-const msg = ref('Hello World!')
+<script setup lang="ts">
+import Home from '@/views/HomeView.vue'
+import Header from '@/components/Header.vue'
+import Background from '@/components/Background.vue'
 </script>
 
 <template>
-  <input v-model="msg">
-
-  <HelloWorld :msg="msg" />
+  <main class="relative bg-gray-100 dark:bg-slate-900 sm:dark:bg-transparent sm:bg-transparent">
+    <Background />
+    <Header />
+    <Home />
+  </main>
 </template>

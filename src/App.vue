7898054f2e143c22842aea1a4c3cc953de48435e
dev-tiqra <dev.tiqra@gmail.com>
2023-07-24 16:27:31 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <input v-model="msg">
+
+  <HelloWorld :msg="msg" />
+</template>
